e4f3314374c007fbb16ee14258fab898c13eeb35
Satish Chinthanippu <satish.chinthanippu@teradata.com>
2025-07-10 00:17:08 -0700

    Updated with other branches

--- plugin/trino-teradata/src/test/java/io/trino/plugin/teradata/TeradataJdbcConnectorTest.java
+++ plugin/trino-teradata/src/test/java/io/trino/plugin/teradata/TeradataJdbcConnectorTest.java
@@ -6,6 +6,12 @@ import io.trino.testing.TestingConnectorBehavior;
 import io.trino.testing.sql.SqlExecutor;
 import org.junit.jupiter.api.AfterAll;
 
+import java.util.OptionalInt;
+
+import static io.trino.plugin.teradata.util.TeradataConstants.TERADATA_OBJECT_NAME_LIMIT;
+import static java.lang.String.format;
+import static org.assertj.core.api.Assertions.assertThat;
+
 /**
  * Integration test class for Teradata JDBC Connector.
  * Sets up schema and tables before tests and cleans up afterwards.
@@ -42,8 +48,8 @@ public class TeradataJdbcConnectorTest
                  SUPPORTS_PREDICATE_PUSHDOWN,
                  SUPPORTS_AGGREGATION_PUSHDOWN,
                  SUPPORTS_JOIN_PUSHDOWN,
-                 SUPPORTS_LIMIT_PUSHDOWN,
-                 SUPPORTS_DROP_SCHEMA_CASCADE -> true;
+                 SUPPORTS_LIMIT_PUSHDOWN -> true;
+//                 SUPPORTS_DROP_SCHEMA_CASCADE -> true;
             default -> super.hasBehavior(connectorBehavior);
         };
     }
@@ -62,10 +68,20 @@ public class TeradataJdbcConnectorTest
         return TeradataQueryRunner.builder().addCoordinatorProperty("http-server.http.port", "8080").setInitialTables(REQUIRED_TPCH_TABLES).build();
     }
 
-
     @AfterAll
     public void cleanupTestDatabase()
     {
         database.dropTestDatabaseIfExists();
+    }
+
+    @Override
+    protected OptionalInt maxSchemaNameLength()
+    {
+        return OptionalInt.of(TERADATA_OBJECT_NAME_LIMIT);
+    }
+
+    protected void verifySchemaNameLengthFailurePermissible(Throwable e)
+    {
+        assertThat(e).hasMessage(format("Schema name must be shorter than or equal to '%s' characters but got '%s'", TERADATA_OBJECT_NAME_LIMIT, TERADATA_OBJECT_NAME_LIMIT + 1));
     }
 }
