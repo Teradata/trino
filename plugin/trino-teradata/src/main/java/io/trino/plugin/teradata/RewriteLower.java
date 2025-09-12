ec73595a1d8644d93d9b9b0e6d673d74fcf70f21
Satish Ch <satish.chinthanippu@teradata.com>
2025-09-12 22:03:26 +0530

    Implement projection function rewriting for LOWER function in Teradata

---
+++ plugin/trino-teradata/src/main/java/io/trino/plugin/teradata/RewriteLower.java
@@ -0,0 +1,59 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.trino.plugin.teradata;
+
+import io.trino.matching.Capture;
+import io.trino.matching.Captures;
+import io.trino.matching.Pattern;
+import io.trino.plugin.base.expression.ConnectorExpressionRule;
+import io.trino.plugin.jdbc.expression.ParameterizedExpression;
+import io.trino.spi.expression.Call;
+import io.trino.spi.expression.ConnectorExpression;
+import io.trino.spi.expression.FunctionName;
+
+import java.util.Optional;
+
+import static io.trino.matching.Capture.newCapture;
+import static io.trino.plugin.base.expression.ConnectorExpressionPatterns.argument;
+import static io.trino.plugin.base.expression.ConnectorExpressionPatterns.argumentCount;
+import static io.trino.plugin.base.expression.ConnectorExpressionPatterns.call;
+import static io.trino.plugin.base.expression.ConnectorExpressionPatterns.expression;
+import static io.trino.plugin.base.expression.ConnectorExpressionPatterns.functionName;
+import static java.lang.String.format;
+
+public class RewriteLower
+        implements ConnectorExpressionRule<Call, ParameterizedExpression>
+{
+    private static final Capture<ConnectorExpression> VALUE = newCapture();
+    public static final FunctionName LOWER_FUNCTION_NAME = new FunctionName("lower");
+    private static final Pattern<Call> PATTERN = call()
+            .with(functionName().equalTo(LOWER_FUNCTION_NAME))
+            .with(argumentCount().equalTo(1))
+            .with(argument(0).matching(expression().capturedAs(VALUE)));
+
+    @Override
+    public Pattern<Call> getPattern()
+    {
+        return PATTERN;
+    }
+
+    @Override
+    public Optional<ParameterizedExpression> rewrite(Call call, Captures captures, RewriteContext<ParameterizedExpression> context)
+    {
+        Optional<ParameterizedExpression> value = context.defaultRewrite(captures.get(VALUE));
+        return value.map(parameterizedExpression -> new ParameterizedExpression(
+                format("LOWER(%s)", parameterizedExpression.expression()),
+                parameterizedExpression.parameters()));
+    }
+}
